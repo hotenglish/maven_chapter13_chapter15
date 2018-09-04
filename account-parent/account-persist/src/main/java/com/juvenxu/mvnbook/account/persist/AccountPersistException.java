b2a9515e177fbdbf5db06ae6b8960f0be61a6114
laimaosheng <hotenglish@outlook.com>
2018-09-04 15:16:03 +0800

    update 1.0.1

--- account-parent/account-persist/src/main/java/com/juvenxu/mvnbook/account/persist/AccountPersistException.java
+++ account-parent/account-persist/src/main/java/com/juvenxu/mvnbook/account/persist/AccountPersistException.java
@@ -2,6 +2,9 @@ package com.juvenxu.mvnbook.account.persist;
 
 public class AccountPersistException extends Exception {
 
+    public AccountPersistException() {
+    }
+
     public AccountPersistException(String message, Throwable cause) {
         super(message, cause);
     }
