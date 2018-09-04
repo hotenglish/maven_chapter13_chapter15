0c8b2ec7c86e9a83f9717bb3b725063351f67c4c
laimaosheng <hotenglish@outlook.com>
2018-09-04 18:31:18 +0800

    test temporarily

--- account-parent/account-persist/src/main/java/com/juvenxu/mvnbook/account/persist/AccountPersistException.java
+++ account-parent/account-persist/src/main/java/com/juvenxu/mvnbook/account/persist/AccountPersistException.java
@@ -2,9 +2,6 @@ package com.juvenxu.mvnbook.account.persist;
 
 public class AccountPersistException extends Exception {
 
-    public AccountPersistException() {
-    }
-
     public AccountPersistException(String message, Throwable cause) {
         super(message, cause);
     }
